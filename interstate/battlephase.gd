f3a719b99cee1170fdf37bc42a87870dd93e1559
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-01-31 03:45:15 -0800

    Add ItemList to scene

--- interstate/battlephase.gd
+++ interstate/battlephase.gd
@@ -3,30 +3,27 @@ extends Node2D
 
 enum battle_state {STATE_BATTLESTART, STATE_PLAYERTURN, STATE_ENEMYTURN, STATE_BATTLEEND}
 
+
 var current_state: int = 0
 var rng = RandomNumberGenerator.new()
 var player_is_dead: bool = false
+var textbox: Node
+var itemlist: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Access values with Name.KEY, prints '5'
-	print(battle_state.STATE_BATTLESTART)
-	# Use dictionary methods:
-	# prints '["STATE_IDLE", "STATE_JUMP", "STATE_SHOOT"]'
-	print(battle_state.keys())
-	# prints '{ "STATE_IDLE": 0, "STATE_JUMP": 5, "STATE_SHOOT": 6 }'
-	print(battle_state)
-	# prints '[0, 5, 6]'
-	print(battle_state.values())
+	textbox = $TextEdit
+	itemlist = $TextEdit/ItemList
+	itemlist.position = textbox.size / 2 - (itemlist.size / 2)
 	
 	if current_state == battle_state.STATE_BATTLESTART:
-		print("Let's get ready to rumble!!!")
+		textbox.text = "Let's get ready to rumble!!!"
 		print("player's turn")
 		current_state = battle_state.STATE_PLAYERTURN
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta: float) -> void:	
 	if Input.is_action_just_pressed("ui_accept") and current_state != battle_state.STATE_BATTLEEND:
 		var ran = rng.randi_range(0, 3)
 		if current_state == battle_state.STATE_PLAYERTURN:
@@ -53,3 +50,16 @@ func _process(delta: float) -> void:
 	if current_state == battle_state.STATE_BATTLEEND:
 		get_tree().quit()
 	
+
+
+func _on_item_list_item_selected(index: int, extra_arg_0: bool, extra_arg_1: bool) -> void:
+	print(index)
+	print("Arg1" + str(extra_arg_0))
+	print("Arg2" + str(extra_arg_1))
+
+
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	print(index)
+	print(at_position)
+	print(mouse_button_index)
+	itemlist.deselect_all()
